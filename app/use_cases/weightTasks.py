664ba51166e5fbd96e0ee5b52a40481e9d8c217e
gabriribeiro5 <60770018+gabriribeiro5@users.noreply.github.com>
2021-09-20 22:01:05 -0300

    refactor(main-logic): Esse dumpainda  ta bem burro mas a logica eh essa

--- app/use_cases/weightTasks.py
+++ app/use_cases/weightTasks.py
@@ -1,28 +1,11 @@
 '''apply weight to micro-tasks and gennerate a performance rate
 
 CODE SOURCE:
-    ruamel.yaml:
-    - How to update yaml file [https://stackoverflow.com/questions/28557626/how-to-update-yaml-file-using-python]
-    - 
+    - How to update yaml file [https://stackoverflow.com/questions/68617796/how-to-update-the-values-of-yaml-file-using-python-code]
+
 '''
 
-# import os
-# import sys
-# from typing import Counter
-# from collections import Counter
 import yaml
-# import ruamel_yaml
-
-# file_name = 'input.yaml'
-# config, ind, bsi = ruamel_yaml.util.load_yaml_guess_indent(open(file_name))
-
-# instances = config['instances']
-# # instances[0]['host'] = '1.2.3.4'
-# # instances[0]['username'] = 'Username'
-# # instances[0]['password'] = 'Password'
-
-# with open('output.yaml', 'w') as fp:
-#     yaml.dump(config, fp)
 
 #Set external variables
 inputFilesPath = r'c:/Users/User/true_performance/app/data/input/' # r'path' turns path-like object into raw string
@@ -38,33 +21,38 @@ commonTasksPathName = f'{outputFilesPath}{commonTasksFile}'
 configFilePathName = f'{configFilesPath}{configFileName}'
 print("Looking for setbacks at: "+setbacksPathName)
 
+def loadDataFrom(filePathName):
+    with open(file=filePathName, mode='r', encoding='utf-8') as anyFile:
+        print(f"Loading data from {filePathName}")
+        fileData = yaml.safe_load(anyFile)
+        print("Closing refered file")
+        anyFile.close()
+        return fileData
+
+def updateFile(newData, filePathName):
+    with open(file=filePathName, mode='w', encoding='utf-8') as anyFile:
+        print(f"Dumping data into target file")
+        yaml.dump(newData, anyFile)
 
 def weightTransactions(srcFilePathName, targetFilePathName):
     'distributes wheight to each transaction in configfiles.commonTasks acording to configfiles.setbacks'
     print("")
     print("================ Applying wheight to commonTasks ================")
     print("")
+
     print(f"Opening source file")
-    with open(file=srcFilePathName, mode='r', encoding='utf-8') as stbkFile:
-        print(f"Loading source file data to memory")
-        setbacks = yaml.safe_load(stbkFile)
-        print(f">> first setback is: {setbacks['STBK1']['DESCRIPTION']}")
-        print("Closing source file")
-        stbkFile.close()
+    setbacksData = loadDataFrom(srcFilePathName)
+    print(f">> first setback is: {setbacksData['STBK1']['DESCRIPTION']}")
+    
+    print("-----------------------------------------------------------------")
+
+    print(f"Opening target file")
+    tasksData = loadDataFrom(targetFilePathName)
+    print(f">> first task is: {tasksData['INTERNATIONAL']}")
 
     print("-----------------------------------------------------------------")
 
-    with open(file=targetFilePathName, mode='r', encoding='utf-8') as tasksFile:
-        print(f"Loading target file to memory")
-        tasksData = yaml.safe_load(tasksFile)
-        print("Closing target file")
-        tasksFile.close()
-
-    print("-----------------------------------------------------------------")
-
-    with open(file=targetFilePathName, mode='w', encoding='utf-8') as tasksFile:
-        print(f"Dumping data into target file")
-        yaml.dump(tasksData, tasksFile)
+    updateFile(tasksData, targetFilePathName)
 
     print("==================================================================")
             
