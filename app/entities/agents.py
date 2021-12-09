834022479e88fb68e53bf8aba121ab251171df35
Gabriel Ribeiro Emegildio <60770018+gabriribeiro5@users.noreply.github.com>
2021-12-09 19:06:59 -0300

    feat(entities.agents): add Rate variable to each agent class and switches Transactions to Tasks on commonTasks.py comments

--- app/entities/agents.py
+++ app/entities/agents.py
@@ -1,26 +1,27 @@
-''' 
+'''
 This file defines the active entities of the application
 Those entities are the ones who initialize a transaction and therefore
 the ones who are going to receive intermediate and final KPI's
 '''
 
-class ConsultantClass():
-    '''Travel Agent'''
+class SingleAgentClass():
+    '''The person associated to a given Task'''
 
-    consultantId: str
-    consultantPosition: str #Junior, Pleno ou Senior
+    agentId: str
+    agentPosition: str #Junior, Pleno ou Senior
 
     mainAssignment: str # Principais atribuições do consultor (Nac, Inter, Nac+Inter, Multiplicador, Emissão)
     taskList: dict
-    
-    associatedAccountIds: dict # Valores de accountId atualmente associados ao consultor
-    associatedAccountIds
 
-    
+    associatedAccountIds: dict # Valores de accountId atualmente associados ao consultor
+
+    agentRate: int # A target variable, represents the agent’s final score
+
 
 class AccountClass():
-    '''Client group, served company''' 
-    
+    '''Client group, served company
+    It’s score is updated whenever an associated single agent gets new points'''
+
     accountId: str
     taskList: dict
 
@@ -30,15 +31,19 @@ class AccountClass():
     associatedBranchOfficeIds: dict # Geralmente cada conta contém apenas uma chave ('ID') e valor ('int')
     serviceType: str # OBT ou FULLSERVICE (Offline)
 
-    
+    accountRate: int # A target variable, represents the agent’s final score
+
+
 class BranchOfficeClass():
     '''PT-BR: Filial'''
-    
+
     branchId: str
     taskList: dict
 
+    branchOfficeRate: int # A target variable, represents the agent’s final score
+
 class BusinessUnityClass():
-    '''Sales Representative
+    '''Third Party Sales Representative
     PT-BR: Representantes comerciais (terceiros)'''
 
     businessUnityId: str
