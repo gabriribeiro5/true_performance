551b2acda279dce8d56836a2d9d8882668043640
Gabriel Ribeiro Emegildio <60770018+gabriribeiro5@users.noreply.github.com>
2021-12-11 20:41:34 -0300

    feat(loadDataFrom-and-updateFile): features once in weightTasks.py are moved to utils

---
+++ app/presenters/updateRateRecord.py
@@ -0,0 +1,4 @@
+''' Capture agent data from applyRateToAgent
+Connect to data record endpoin
+Locate agent’s recording adress
+Save new agentId.yaml according to the operator pointed in the source'''
