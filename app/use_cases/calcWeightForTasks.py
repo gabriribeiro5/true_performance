e35cd7240f3737edcc54bf990b024fbb75c68a6c
Gabriel Ribeiro Emegildio <60770018+gabriribeiro5@users.noreply.github.com>
2021-12-11 20:20:56 -0300

    feat(weight_tasks_class - not working yet): new version of weightTasks.py, should provide more control and flexibility over the code functionalities

--- app/use_cases/calcWeightForTasks.py
+++ app/use_cases/calcWeightForTasks.py
@@ -1,4 +1,79 @@
 '''
-Use variables from controller.getEntryVariables
-weight=(subweight1 + subweight2 + subweight3)/subtasksCount
+Use variables from data.input.setbacks.yaml to calc
+    additionalWeight=(subweight1 + subweight2 + subweight3)/subtasksCount
+
+Apply additionalWeight to data.output.commonTasks.yaml
 '''
+
+from utils import fileManager
+import yaml
+
+#Set external variables
+inputFilesPath = r'/home/kraken/codes/true_performance/app/data/input/' # r'path' turns path-like object into raw string
+outputFilesPath = r'/home/kraken/codes/true_performance/app/data/output/'
+configFilesPath  = r'/home/kraken/codes/true_performance/app/configfiles/'
+setbacksFile = 'setbacks.yaml'
+commonTasksFile = 'commonTasks.yaml'
+configFileName = 'conf.yaml'
+
+class weight_tasks_class(object, taskCategory, taskName, srcFilePathName, targetFilePathName):
+    """taskCategory:
+        taskName:
+        srcFilePathName:
+        targetFilePathName:
+    """
+
+    def __init__(self, arg, taskType):
+        super(weight_tasks_class, self).__init__()
+        self.arg = arg
+        self.taskType = taskType
+        self.srcFilePathName = srcFilePathName
+        self.targetFilePathName = targetFilePathName
+        self.setbacksPathName = f'{inputFilesPath}{setbacksFile}' # f'{var1}{var2}' allows concatenating variables into str mode
+        self.commonTasksPathName = f'{outputFilesPath}{commonTasksFile}'
+        self.configFilePathName = f'{configFilesPath}{configFileName}'
+        print("Looking for setbacks at: "+self.setbacksPathName)
+
+    def loadDataFrom(filePathName):
+        """
+        filePathName: full path and name of the file to be loaded
+
+        """
+        with open(file=filePathName, mode='r', encoding='utf-8') as anyFile:
+            print(f"Loading data from {filePathName}")
+            fileData = yaml.safe_load(anyFile)
+            print("Closing refered file")
+            anyFile.close()
+            return fileData
+
+    def updateFile(newData, filePathName):
+        with open(file=filePathName, mode='w', encoding='utf-8') as anyFile:
+            print(f"Dumping data into target file")
+            yaml.dump(newData, anyFile)
+
+    print(f"Opening source file")
+    setbacksData = loadDataFrom(srcFilePathName)
+
+    print(f"Opening target file")
+    tasksData = loadDataFrom(targetFilePathName)
+
+    def addUpWeight(setbacksData, tasksData):
+        """
+        filePathName: full path and name of the file to be loaded
+
+        """
+        print(f"Adding setbacks weight to commonTasksFile")
+        for stbk in setbacksData:
+            if taskName in stbk['RELATED_TASK_CATEGORY']:
+                for subtask in stbk['RELATED_SUBTASKS']:
+                    tasksData[{taskCategory}][{taskName}][WEIGHT] += stbk['WEIGHT']
+
+    print("-----------------------------------------------------------------")
+
+    updateFile(tasksData, targetFilePathName)
+    loadDataFrom()
+
+    print("==================================================================")
+
+
+# performanceRate = resultingWeight * stbk['RECURRENCY']
