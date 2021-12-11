cef317229dcd28e3e5d3602c614653ecfd10d394
Gabriel Ribeiro Emegildio <60770018+gabriribeiro5@users.noreply.github.com>
2021-12-11 20:33:59 -0300

    feat(frontend-and-backend-communication-files): add feature files for future development purpose
    Following compliance with Clean Archtecture principles

--- app/presenter/returnPreviewWheights.py
+++
@@ -1,4 +0,0 @@
-''' Get results from CalcWheight and return it to UI
-It’s a preview of all inputs result
-=(subweight1 + subweight2 + subweight3)/subtasksCount 
-'''
--- app/presenter/updateRateRecord.py
+++
@@ -1,4 +0,0 @@
-''' Capture agent data from applyRateToAgent
-Connect to data record endpoin
-Locate agent’s recording adress
-Save new agentId.yaml according to the operator pointed in the source'''
